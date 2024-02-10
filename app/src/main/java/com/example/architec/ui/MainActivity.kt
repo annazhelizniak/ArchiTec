17494c6b56617c4f205d4f756395b27bceb41ea7
Anna Zhelizniak <anna.zhelizniak08@gmail.com>
2024-02-10 20:54:00 +0200

    Revert "wrong changes"
    This reverts commit 5eeb3de93cf41a0f91194e08195270d3a742a685.

--- app/src/main/java/com/example/architec/ui/MainActivity.kt
+++ app/src/main/java/com/example/architec/ui/MainActivity.kt
@@ -25,26 +25,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.Theme_ArchiTec_NoActionBar)
         setContentView(binding.root)
-        //setSupportActionBar(binding.appBarMain.toolbar)
+        setSupportActionBar(binding.appBarMain.toolbar)
 
-//        binding.appBarMain.fab?.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//        }
+        binding.appBarMain.fab?.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
 
         val navHostFragment =
                 (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment?)!!
         val navController = navHostFragment.navController
 
-//        binding.navView?.let {
-//            appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow, R.id.nav_settings
-//            ),
-//                    binding.drawerLayout
-//            )
-//            setupActionBarWithNavController(navController, appBarConfiguration)
-//            it.setupWithNavController(navController)
-//        }
+        binding.navView?.let {
+            appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow, R.id.nav_settings
+            ),
+                    binding.drawerLayout
+            )
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            it.setupWithNavController(navController)
+        }
 
         binding.appBarMain.contentMain.bottomNavView?.let {
             appBarConfiguration = AppBarConfiguration(setOf(
@@ -55,18 +55,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        val result = super.onCreateOptionsMenu(menu)
-//        // Using findViewById because NavigationView exists in different layout files
-//        // between w600dp and w1240dp
-//        val navView: NavigationView? = findViewById(R.id.nav_view)
-//        if (navView == null) {
-//            // The navigation drawer already has the items including the items in the overflow menu
-//            // We only inflate the overflow menu if the navigation drawer isn't visible
-//            menuInflater.inflate(R.menu.overflow, menu)
-//        }
-//        return result
-//    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val result = super.onCreateOptionsMenu(menu)
+        // Using findViewById because NavigationView exists in different layout files
+        // between w600dp and w1240dp
+        val navView: NavigationView? = findViewById(R.id.nav_view)
+        if (navView == null) {
+            // The navigation drawer already has the items including the items in the overflow menu
+            // We only inflate the overflow menu if the navigation drawer isn't visible
+            menuInflater.inflate(R.menu.overflow, menu)
+        }
+        return result
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
